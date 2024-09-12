0ccbf53550b5ad222b47aa9b1aa40b4cd920a760
Gentlestore <gentlestore6@gmail.com>
2024-09-12 16:01:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ if (fs.existsSync('config.env')) {
 const API = "https://api-aswin-sparky.koyeb.app";
 const ALIVE = process.env.ALIVE || "I am Alive";
 const HANDLERS = process.env.HANDLER || ".";
-const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:87c4e719313999ef5e91c2cb13b3aa18";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:dbdbb59c312e31f6642365d8c56d4367";
 const SUDO = process.env.SUDO || '917012984396';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "true";
